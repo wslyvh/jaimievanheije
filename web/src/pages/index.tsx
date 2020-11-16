be2c01905d95abddaa89dc06579a6bdd4c5eb7d9
wslyvh <wslyvh@users.noreply.github.com>
2020-11-16 18:43:41 +0100

    fix modal spaces

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -186,7 +186,7 @@ export default function Index({ data }) {
                     type="button"
                     className="btn btn-danger btn-lg margin-button"
                     data-toggle="modal"
-                    data-target={`#${node.frontmatter.title.replace(" ", "")}`}
+                    data-target={`#${node.frontmatter.title.replace(/ /g,'')}`}
                   >
                     {node.frontmatter.title}
                   </button>
@@ -202,7 +202,7 @@ export default function Index({ data }) {
           <div
             key={`${node.id}-modal`}
             className="modal fade"
-            id={`${node.frontmatter.title.replace(" ", "")}`}
+            id={`${node.frontmatter.title.replace(/ /g,'')}`}
             tabIndex={-1}
             role="dialog"
             aria-labelledby={`${node.frontmatter.title}-label`}
