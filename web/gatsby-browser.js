be34ce36546e96e6d64ca2dd48c52de1fa9a06bf
wslyvh <wslyvh@users.noreply.github.com>
2021-05-18 11:31:09 +0200

    cdn scripts

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -8,10 +8,10 @@ const addScript = url => {
 export const onClientEntry = () => {
   console.log("importing custom javascript...")
   window.onload = () => {
-    addScript('https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js')
+    addScript('https://code.jquery.com/jquery-1.11.1.min.js')
     addScript("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js")
-    addScript('https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js')
     addScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCNHRJO3pFmcuw1T8RLthkmH7H7JXaA_cw')
+    addScript('/scripts/jquery.easing.js')
     addScript('/scripts/jaimievanheije.js')
   }
 }
