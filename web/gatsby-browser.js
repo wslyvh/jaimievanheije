700f731a890b36bf19903b03fd34d76745c4ccaa
wslyvh <wslyvh@users.noreply.github.com>
2021-07-09 09:25:11 +0200

    scripts

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -4,12 +4,15 @@ const addScript = url => {
   script.async = true
   document.body.appendChild(script)
 }
+export const onClientEntry = () => {
+  console.log("onClientEntry..")
+  addScript('/scripts/jquery-1.11.1.min.js')
+}
 
 export const onInitialClientRender = () => {
-  console.log("importing custom javascript...")
+  console.log("onClientEntry..")
   window.onload = () => {
     addScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCNHRJO3pFmcuw1T8RLthkmH7H7JXaA_cw')
-    addScript('/scripts/jquery-1.11.1.min.js')
     addScript('/scripts/bootstrap.min.js')
     addScript('/scripts/jquery.easing.js')
     addScript('/scripts/jaimievanheije.js')
